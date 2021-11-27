9ed9cabed7831d3729a3fdfea1384f77af62209b
level237 <level237>
2021-11-27 04:20:04 +0100

    methods of displaying a player's cards and displaying his number of cards

--- src/User/Player/Player.java
+++ src/User/Player/Player.java
@@ -11,11 +11,11 @@ public class Player implements UserInterFace{
 
 	private List <Card> CardOfPlayer;
 	private String Name;
-	private int NumberOfDraw;
+	private int MaximumPickOut;
 	public Player(String Name) {
 		// TODO Auto-generated constructor stub
 		this.CardOfPlayer=new ArrayList <Card>();
-		this.NumberOfDraw=5;
+		this.MaximumPickOut=5;
 		this.Name=Name;
 		
 	}
@@ -25,7 +25,25 @@ public class Player implements UserInterFace{
 		this.CardOfPlayer.add(pack.TakeFirstCardOfTheList());
 		pack.RemoveCardOfTheList(1);
 		System.out.println(pack.RestOftheCard());
+		this.MaximumPickOut--;
 		}
+	public void TravelCardofPlayer() {
+		for(int i=0;i<this.CardOfPlayer.size();i++) {
+			
+			System.out.println(this.CardOfPlayer.get(i).ToString());
+		}
+	}
+	
+	
+	//getter 
+	public int getMaximumPickOut() {
+		return this.MaximumPickOut;
+	}
+	public void getCardOfPlayer() {
+		System.out.println("Card Of Player "+this.Name+"------------------------------------");
+		this.TravelCardofPlayer();
+	}
+	
 	
 
 	}
