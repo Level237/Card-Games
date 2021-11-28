7b004eae2cd0ee02a4a5a2d4e10766080bd0b8ff
level237 <level237>
2021-11-28 22:53:44 +0100

    initialization attributes of the CardField class

--- src/Card/Field/CardField.java
+++ src/Card/Field/CardField.java
@@ -1,10 +1,23 @@
 package Card.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 import Card.Card;
+import Card.Package.CardPackage;
+import User.Player.Player;
 public class CardField {
-	private Card CardOfTheCenter;
+	private List <Card> CardOfTheCenter;
+	private Player player;
+	private CardPackage PackCard;
 	
-	public CardField() {
+	public CardField(CardPackage PackCard,Player player) {
 		// TODO Auto-generated constructor stub
+		this.PackCard=PackCard;
+		this.player=player;
+		this.CardOfTheCenter=new ArrayList<Card>();
+		
+	}
+	public void initCenter() {
 		
 	}
 	
